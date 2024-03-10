39d1c76d19bddbfb06177b56d7de35cc39cda6ef
ITS ASHISH <162326526+AshishMohapatra03@users.noreply.github.com>
2024-03-10 13:00:12 +0530

    Add files via upload

---
+++ ashish.py
@@ -0,0 +1,25 @@
+a=True
+b=False
+print(a)
+print(b)
+
+a=50
+b=60
+result=a>b
+print(result)
+c=True
+print(type(c))
+a= "i am python class"
+print(a)
+b='ashish'
+print(b)
+c='''gudu'''
+print(c)
+address="""plot no 270k7 kalinganagar pin-754141"""
+print(address)
+a=7
+b=8
+int=a+b
+print(int)
+x = "22 22"
+print(float(x))
